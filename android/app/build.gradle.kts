2b2c08114131e49292da09e307031b15e94491c5
Taha Moeini <taha.moeini@gmail.com>
2025-12-14 01:06:45 +0330

    chore: Update compileSdk version to 35 for improved compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.debt_manager"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
